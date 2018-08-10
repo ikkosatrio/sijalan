de34fca4331a6897529a48a9bb989b701fd2c299
ASUS-PC\Ikko <ikkosat>
2018-08-11 00:05:29 +0700

    no message

--- application/views/main/ruasjalan/info_ruas.blade.php
+++ application/views/main/ruasjalan/info_ruas.blade.php
@@ -28,8 +28,6 @@ Dashboard - Administrasi
 						<div class="heading-elements">
 							<ul class="icons-list">
 		                		<li><a data-action="collapse"></a></li>
-		                		<li><a data-action="reload"></a></li>
-		                		<li><a data-action="close"></a></li>
 		                	</ul>
 	                	</div>
 					</div>
